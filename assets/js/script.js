0521e955354439c87a68d5d7e357b830b0739420
Joh201 <johngt632@gmail.com>
2021-08-20 16:07:40 +0000

    Remove commented out codes from the js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,19 +3,12 @@
  * Getting DOM elements and declaring constants
  */
 const buttons = document.getElementsByClassName('buttons');
-
 const playerScore = document.getElementById('player-score');
-
 const computerScore = document.getElementById('computer-score');
-
 const playerImage = document.getElementById('player-image');
-
 const computerImage = document.getElementById('computer-image');
-
-const message = document.getElementById('message')
-
-const motivationMessage = document.getElementById('motivation-message')
-
+const message = document.getElementById('message');
+const motivationMessage = document.getElementById('motivation-message');
 const options = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
  
 
@@ -26,48 +19,41 @@ const options = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
   for(let button of buttons){
     button.addEventListener('click', function(){
     let playerOption = this.getAttribute('data-type');
-    console.log(typeof(playerOption))
-        // alert(`You clicked${playerOption}`)
         playGame(playerOption);
     });
-}
+  }
 
 
 /**
- * The game function takes the value of the data-type of the clicked button as a parameter
+ * The playGame function takes the value of the data-type of the clicked button as a parameter
  */
 
 function playGame(playerOption){
 
           playerImage.src = `assets/images/${options[playerOption]}.png`;
-          console.log(playerImage.src)
           playerImage.alt = options[playerOption];
-          console.log(playerImage.alt)
 
-        let computerOption = Math.floor(Math.random()*5)
+        const computerOption = Math.floor(Math.random()*5);
 
         computerImage.src = `assets/images/${options[computerOption]}.png`;
-        console.log(computerImage.src)
     
         computerImage.alt = options[computerOption];
 
-         let result = determineWinner(options[playerOption], options[computerOption]) 
-
-         console.log(result)
+        const result = determineWinner(options[playerOption], options[computerOption]); 
         
         updatePoints(result);       
 }
 
 
+/** The determineWinner function takes two parameters from the array and determines the winner*/
 function determineWinner( computerOption, playerOption){
 
   computerOption = computerImage.alt;
   playerOption =  playerImage.alt;
 
   if(computerOption === playerOption){
-    return 'It is a tie!'
-    // message.textContent = 'It is a tie'
-    // return message
+    return 'It is a tie!';
+    
   } else if (computerOption !== playerOption){
     if(computerOption === options[0] && playerOption === options[2] ||
       computerOption === options[0] && playerOption === options[3] ||
@@ -79,36 +65,35 @@ function determineWinner( computerOption, playerOption){
       computerOption === options[1] && playerOption === options[4] ||
       computerOption === options[3] && playerOption === options[1] ||
       computerOption === options[3] && playerOption === options[4]){
-        return 'Computer wins!'
-        // message.textContent = 'Computer wins!'
-        // return message
+        return 'Computer wins!';
+
     } else {
-        return 'You win!'
-        // message.textContent = 'You win!'
-        // return message
+        return 'You win!';
     }
   }
 }
 
+/** This function takes the outcome of the determineWinner function as a parameter 
+ * and updates the scores of the player and the computer*/
+
 function updatePoints(result){
 
   if (result === 'Computer wins!'){
-    ++computerScore.textContent
-    console.log(computerScore)
-    message.textContent = 'Computer wins!'
-    motivationMessage.textContent = "Don't give up!"
-    // return message
+    ++computerScore.textContent;
+    message.textContent = 'Computer wins!';
+    motivationMessage.textContent = "Don't give up!";
+    
   } else if (result === 'You win!'){
     ++playerScore.textContent;
-    console.log(playerScore)
-     motivationMessage.textContent = ''
-     message.textContent = 'You win!'
-    //  return message
+     motivationMessage.textContent = '';
+     message.textContent = 'You win!';
+    
     } else{
-      computerScore.textContent
-      playerScore.textContent
-      motivationMessage.textContent = '' 
-      message.textContent = 'It is a tie!'
+      computerScore.textContent;
+      playerScore.textContent;
+      motivationMessage.textContent = ''; 
+      message.textContent = 'It is a tie!';
     } 
 
 }
+
